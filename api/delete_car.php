21255e733aaccd9ba257d422eff8b2dbb3cecb46
Giedrius Kristinaitis <36892255+Giedrius-Kristinaitis@users.noreply.github.com>
2019-01-31 21:05:20 +0200

    Update delete_car.php

--- api/delete_car.php
+++ api/delete_car.php
@@ -2,7 +2,7 @@
 
 // set required header fields
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: DELETE');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Max-Age: 3600');
